c20ef8fa159cf938de0da2b3c651bbdd1d7db508
Ivan Korniichuk <ivanvan.korniichuk@gmail.com>
2021-06-17 16:17:18 +0300

    3rd edit

--- 5/Assets/Scripts/Alarm.cs
+++ 5/Assets/Scripts/Alarm.cs
@@ -8,28 +8,34 @@ public class Alarm : MonoBehaviour
     [SerializeField] private AudioSource _alarmSound;
     [SerializeField] private float _changeRate;
 
+    private bool _isWorking = false;
     private float _currVolume = 0;
     private float _targetVolume = 1;
 
     private void Update()
     {
-        _currVolume = Mathf.MoveTowards(_currVolume, _targetVolume, (1 / _changeRate) * Time.deltaTime);
+        if (_isWorking)
+        {
+            _currVolume = Mathf.MoveTowards(_currVolume, _targetVolume, (1 / _changeRate) * Time.deltaTime);
 
-        _alarmSound.volume = _currVolume;
+            _alarmSound.volume = _currVolume;
 
-        if (_currVolume == 1)
-            _targetVolume = 0;
-        else if (_currVolume == 0)
-            _targetVolume = 1;
+            if (_currVolume == 1)
+                _targetVolume = 0;
+            else if (_currVolume == 0)
+                _targetVolume = 1;
+        }
     }
 
     public void TurnOnAlarm()
     {
         _alarmSound.Play();
+        _isWorking = true;
     }
 
     public void TurnOffAlarm()
     {
         _alarmSound.Stop();
+        _isWorking = false;
     }
 }
